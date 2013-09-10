7a0b5562bd79ca5850b1a6068a22ac4b12b091e0
Ivan Towlson <github@hestia.cc>
2013-09-11 11:43:10 +1200

    Added some more interop samples

--- TenThingsDemos/CSharpEquivalents/Interop.cs
+++ TenThingsDemos/CSharpEquivalents/Interop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.FSharp.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,37 @@ namespace CSharpEquivalents
             var expr = DiscriminatedUnions.ExprNode.NewVariableRef("myVariable");
             Console.WriteLine(expr);
             Console.WriteLine(((DiscriminatedUnions.ExprNode.VariableRef)expr).Item);
+
+            var fnexpr = DiscriminatedUnions.ExprNode.NewFunctionApplication("increment", new FSharpList<DiscriminatedUnions.ExprNode>(expr, FSharpList<DiscriminatedUnions.ExprNode>.Empty));
+            var fn = (DiscriminatedUnions.ExprNode.FunctionApplication)fnexpr;
+            Console.WriteLine(fn.Name());
+            Console.WriteLine(fn.Argument(0).As<DiscriminatedUnions.ExprNode.VariableRef>().Name());
         }
 
         private static string Name(this DiscriminatedUnions.ExprNode.VariableRef variableRef)
         {
             return variableRef.Item;
+        }
+
+        private static string Name(this DiscriminatedUnions.ExprNode.FunctionApplication fn)
+        {
+            return fn.Item1;
+        }
+
+        private static IEnumerable<DiscriminatedUnions.ExprNode> Arguments(this DiscriminatedUnions.ExprNode.FunctionApplication fn)
+        {
+            return fn.Item2;
+        }
+
+        private static DiscriminatedUnions.ExprNode Argument(this DiscriminatedUnions.ExprNode.FunctionApplication fn, int index)
+        {
+            return fn.Item2.ElementAt(index);
+        }
+
+        private static T As<T>(this DiscriminatedUnions.ExprNode expr)
+            where T : DiscriminatedUnions.ExprNode
+        {
+            return expr as T;
         }
     }
 }
