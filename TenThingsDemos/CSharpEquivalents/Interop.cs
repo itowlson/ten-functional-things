374381c8752de7fe83613a822a587844e45f5738
Ivan Towlson <github@hestia.cc>
2013-09-11 16:16:49 +1200

    Added comments and shout out to FSharpx

--- TenThingsDemos/CSharpEquivalents/Interop.cs
+++ TenThingsDemos/CSharpEquivalents/Interop.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.FSharp.Collections;
+using Microsoft.FSharp.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,21 +10,48 @@ namespace CSharpEquivalents
 {
     public static class Interop
     {
+        // FSharpx (https://github.com/fsharp/fsharpx) has
+        // helpers for Option, Unit, List, functions and more
+
         public static void Test()
         {
-            var pt = new Immutable.Point3D(0.1, 0.2, 0.3);
+            // Records => classes
+            var pt = new Immutable.Point3D(0.1, 0.2, 0.3);  // constructor
             Console.WriteLine(pt);
-            Console.WriteLine(pt.X);
-            Console.WriteLine(Immutable.extend(pt, 0.4, 0.5, 0.6));
+            Console.WriteLine(pt.X);  // member access
 
+            // Module functions => static methods
+            var extended = Immutable.extend(pt, 0.4, 0.5, 0.6);
+            Console.WriteLine(extended);
+
+            // Discriminated unions => base + subclasses
             var expr = DiscriminatedUnions.ExprNode.NewVariableRef("myVariable");
             Console.WriteLine(expr);
             Console.WriteLine(((DiscriminatedUnions.ExprNode.VariableRef)expr).Item);
 
-            var fnexpr = DiscriminatedUnions.ExprNode.NewFunctionApplication("increment", new FSharpList<DiscriminatedUnions.ExprNode>(expr, FSharpList<DiscriminatedUnions.ExprNode>.Empty));
+            // DUs can cross the boundary -- but they can be verbose!
+            // Consider putting helper functions on the F# side
+            var fnexpr = DiscriminatedUnions.ExprNode.NewFunctionApplication("increment", BuildFSharpList(expr));
             var fn = (DiscriminatedUnions.ExprNode.FunctionApplication)fnexpr;
             Console.WriteLine(fn.Name());
             Console.WriteLine(fn.Argument(0).As<DiscriminatedUnions.ExprNode.VariableRef>().Name());
+
+            // Can't use C# delegates for F# function arguments - see
+            // FSharpx for helpers.
+            //FSharpOption<string> bob = FSharpOption<string>.Some("bob");
+            //Option.applyOrDefault(bob, s => "hello " + s, () => "bye");
+        }
+
+        // Dealing with F# lists in C# can be fiddly - give yourself
+        // helper functions (either in C# or in F#)
+        private static FSharpList<T> BuildFSharpList<T>(params T[] values)
+        {
+            var list = FSharpList<T>.Empty;
+            for (int i = values.Length - 1; i >= 0; --i)
+            {
+                list = new FSharpList<T>(values[i], FSharpList<T>.Empty);
+            }
+            return list;
         }
 
         private static string Name(this DiscriminatedUnions.ExprNode.VariableRef variableRef)
